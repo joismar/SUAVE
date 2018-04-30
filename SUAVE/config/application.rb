c5939a6028db89282cc9a12ee88b46fa91841ec1
Hugo Oliveira <holivra@outlook.com>
2018-04-29 21:44:29 -0300

    Gerando testes apenas para models e controllers

--- SUAVE/config/application.rb
+++ SUAVE/config/application.rb
@@ -14,7 +14,11 @@ module SUAVE
 
     # Rspec Config.
     config.generators do |g|
-      g.test_framework :rspec
+      g.test_framework :rspec,
+                        :view_specs => false,
+                        :helper_specs => false,
+                        :routing_specs => false,
+                        :request_specs => false
     end
 
   end
