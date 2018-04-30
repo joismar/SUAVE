a1510ee260ed20cb9f81f19d8dd089c41ce9fd72
Hugo Oliveira <holivra@outlook.com>
2018-04-29 21:36:50 -0300

    Configurando rspec como padrao

--- SUAVE/config/application.rb
+++ SUAVE/config/application.rb
@@ -11,5 +11,11 @@ module SUAVE
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Rspec Config.
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+
   end
 end
