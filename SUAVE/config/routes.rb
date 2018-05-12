a6068e5e68c82ca52a42b6f02229c9c7430b55f0
Joismar <joismar@outlook.com>
2018-05-11 23:42:42 -0300

    Adicionando rotas da página "sobre"

--- SUAVE/config/routes.rb
+++ SUAVE/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
+  get "sobre", to: "sobre#sobre"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
