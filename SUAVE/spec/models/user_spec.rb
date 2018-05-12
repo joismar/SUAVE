5c218f44d29ce1a727ee720e5e83503a566f1a97
Hugo Oliveira <holivra@outlook.com>
2018-05-12 09:19:28 -0300

    Teste para criação de usuario no BD.

--- SUAVE/spec/models/user_spec.rb
+++ SUAVE/spec/models/user_spec.rb
@@ -18,4 +18,28 @@ RSpec.describe User, type: :model do
     usuario = User.new(password: nil)
     expect(usuario).to_not be_valid
   end
+
+  it "Usuario criado no B, com todos os atributos" do
+    usuario = User.create(email: "james@email.com",
+                       password: "123456",
+                       password_confirmation: "123456")
+
+    novo = User.first
+    expect(novo.email).to eq("james@email.com")
+  end
+
+  it "Usuario não criado no BD, sem email" do
+    usuario = User.create(email: nil,
+                       password: "123456",
+                       password_confirmation: "123456")
+
+    expect(User.first).to eq(nil)
+  end
+
+  it "Usuario não criado no BD, sem senha" do
+    usuario = User.create(email: "james@email.com",
+                       password: nil)
+
+    expect(User.first).to eq(nil)
+  end
 end
