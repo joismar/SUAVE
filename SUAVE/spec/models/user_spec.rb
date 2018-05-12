49cccfc5487684eb30e721586670e5d8a5a3a72d
Hugo Oliveira <holivra@outlook.com>
2018-05-12 02:28:57 -0300

    Corrigindo testes Rspec, agora para o model User.

--- SUAVE/spec/models/user_spec.rb
+++ SUAVE/spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "Usuario valido com todos os atributos" do
+    usuario = User.new(email: "james@email.com",
+                       password: "123456",
+                       password_confirmation: "123456")
+
+    expect(usuario).to be_valid
+  end
+
+  it "Usuario invalido sem email" do
+    usuario = User.new(email: nil)
+    expect(usuario).to_not be_valid
+  end
+
+  it "Usuario invalido sem senha" do
+    usuario = User.new(password: nil)
+    expect(usuario).to_not be_valid
+  end
 end
