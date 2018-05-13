0e17eef1dee4402d9c3d606c7a60f5a3d4b0337a
Hugo Oliveira <holivra@outlook.com>
2018-05-13 17:40:43 -0300

    Corringindo rotas.

--- SUAVE/config/routes.rb
+++ SUAVE/config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   get 'home/index'
   post 'home/index'
   root 'home#index'
   get "sobre", to: "sobre#sobre"
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 end
