6bd29bb6bcf185f5f5486e5c07fd1ec191f02673
Jcns <jcns@cin.ufpe.br>
2018-05-15 01:27:10 -0300

    Altera valor de Logado como para nome do usuario

--- SUAVE/app/models/user.rb
+++ SUAVE/app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+       user.name = auth.info.name
        user.provider = auth.provider
        user.uid = auth.uid
        user.email = auth.info.email
