c2a0c3e69a8d2208a9b89d804cf22c3f640e9e19
Jcns <jcns@cin.ufpe.br>
2018-05-17 02:53:21 -0300

    Configurando mudança de senha

--- SUAVE/config/environments/development.rb
+++ SUAVE/config/environments/development.rb
@@ -27,7 +27,8 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.perform_caching = false
 
@@ -52,5 +53,16 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { :host => 'sisuave.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.smtp_settings =
+  {
+
+    :address            => 'smtp.gmail.com',
+    :port               => 587,
+    :domain             => 'localhost',
+    :authentication     => 'plain',
+    :user_name          => 'noreply.suave@gmail.com',
+    :password           => 'suaveevaus',
+    :enable_starttls_auto => true
+  }
 end
