1383dc6a3d159e082feb5a540fc2eecd1161a633
Joismar <joismar@outlook.com>
2018-05-19 13:01:32 -0300

    Realizando algumas modificações em home e escolas

--- SUAVE/config/routes.rb
+++ SUAVE/config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   post 'home/index'
   root 'home#index'
   get "sobre", to: "sobre#sobre"
-
+  get "escola", to: "escola#escola"
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 end
