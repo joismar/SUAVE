3d55f7e4ff122dc62ade38530a1f469726e87ffe
Hugo Oliveira <holivra@outlook.com>
2018-06-17 18:22:06 -0300

    Notas ENEM

--- SUAVE/app/controllers/stats_controller.rb
+++ SUAVE/app/controllers/stats_controller.rb
@@ -2,6 +2,11 @@ class StatsController < ApplicationController
   def stats
     estatisticas = Stat.new
     @brasil = estatisticas.brasil
+    @norte = estatisticas.norte
+    @nordeste = estatisticas.nordeste
+    @centro = estatisticas.centro
+    @sudeste = estatisticas.sudeste
+    @sul = estatisticas.sul
 
   end
 end
